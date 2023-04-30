866ca45b80111183ed526fbb3b026deb4ec3611b
kbjcx <lu.kbjcx@gmail.com>
2023-04-30 21:51:29 +0800

    change

--- main.cpp
+++ main.cpp
@@ -55,5 +55,46 @@ int main(int argc, char* argv[]) {
     AVPacket* packet;
     uint8_t* out_buffer;
     
+    static struct SwsContext* img_convert_context;
+    int video_stream, i, num_bytes;
+    int ret, got_picture;
+    
+    // av_register_all();
+    
+    // allocate an AVFormatContext
+    format_context = avformat_alloc_context();
+    
+    if (avformat_open_input(&format_context, file_path, nullptr, nullptr) != 0) {
+        printf("can't open the file");
+        return -1;
+    }
+    
+    if (avformat_find_stream_info(format_context, nullptr) < 0) {
+        printf("can't find stream information");
+        return -1;
+    }
+    video_stream = -1;
+    
+    // 循环查找视频中包含的流信息, 直到找到视频类型的流
+    // 将其记录下来, 保存到video_stream变量中
+    // 这里只处理视频流, 不管音频流
+    for (i = 0; i < format_context->nb_streams; ++i) {
+        if (format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            video_stream = i;
+        }
+    }
+    
+    // 如果video_stream = -1 说明没有找到视频流
+    if (video_stream == -1) {
+        printf("didn't find a video stream");
+        return -1;
+    }
+    
+    // 查找解码器
+    codec_context = format_context->streams[video_stream]->codecpar;
+    
+    
+    
+    
     return QApplication::exec();
 }
